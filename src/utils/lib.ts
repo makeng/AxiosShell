118ee2cb2a0b98cd383863b849cfefa81fef2845
马兆铿 <810768333@qq.com>
2026-03-19 09:24:42 +0800

    🧹: Clean up code comments and add lib module

---
+++ src/utils/lib.ts
@@ -0,0 +1,23 @@
+/* ---------------------------------------------------------------------------------------
+ * about:类型判断和各种基础函数。只增加必要的
+ * ---------------------------------------------------------------------------------------- */
+
+const { toString } = Object.prototype;
+
+function isObject(x: unknown): x is Record<string, unknown> {
+  return !!x && toString.call(x) === '[object Object]';
+}
+
+function isArray<T = unknown>(x: unknown): x is T[] {
+  return toString.call(x) === '[object Array]';
+}
+
+function isString(x: unknown): x is string {
+  return !!x && toString.call(x) === '[object String]';
+}
+
+function isFunction(x: unknown): x is (...args: unknown[]) => unknown {
+  return toString.call(x) === '[object Function]';
+}
+
+export { isObject, isArray, isString, isFunction };
